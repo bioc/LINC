d45913137e0c7651b3dbe5f024d704dbc5b57860
Manuel Goepferich <manuel.goepferich@gmx.de>
2016-09-26 14:13:07 +0000

    Update test_LINC.R
    From: ManuelGoepferich <manuel.goepferich@gmx.de>
    
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/LINC@121383 bc3139a8-67e5-0310-9ffc-ced21a209358

--- inst/unitTests/test_LINC.R
+++ inst/unitTests/test_LINC.R
@@ -54,18 +54,18 @@ test_pvalueCluster <- function() {
 }
 
 # 4 queries in singlelinc
-#test_QueryofSingle <- function() {
+test_QueryofSingle <- function() {
   
-#  cor_test_mat <- matrix(c(c(1:110), NA, NA, 50, Inf, 3,
-#                           NaN, 0.5, NA, 80, 5, 4, c(1:50), rep(NA, 14), rep(9, 15)),
-#                         ncol = 10, byrow = TRUE)
-#  rownames(cor_test_mat) <- as.character(c(1:20))
-#  colnames(cor_test_mat) <- LETTERS[1:10]
+  cor_test_mat <- matrix(c(c(1:110), NA, NA, 50, Inf, 3,
+                           NaN, 0.5, NA, 80, 5, 4, c(1:50), rep(NA, 14), rep(9, 15)),
+                         ncol = 10, byrow = TRUE)
+  rownames(cor_test_mat) <- as.character(c(1:20))
+  colnames(cor_test_mat) <- LETTERS[1:10]
   
-#  linc_matrix <- linc(cor_test_mat, codingGenes = c(TRUE, FALSE,
-#                                                    TRUE, TRUE, FALSE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE,
-#                                                    TRUE, TRUE, rep(FALSE, 7)))
-#  res <-  singlelinc(linc_matrix, query = "17", onlycor = T, underth = F, threshold = 0.99)
-#  checkEquals(res@results$query, "17")
-#}
+  linc_matrix <- linc(cor_test_mat, codingGenes = c(TRUE, FALSE,
+                                                    TRUE, TRUE, FALSE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE,
+                                                    TRUE, TRUE, rep(FALSE, 7)))
+  res <-  singlelinc(linc_matrix, query = "17", onlycor = T, underth = F, threshold = 0.99)
+  checkEquals(res@results$query, "17")
+}
 
